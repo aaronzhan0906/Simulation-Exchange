0060992392d0e0b33081bf8235f03c693288b43c
ChengTing <aaronzhan0906@gmail.com>
2024-08-16 00:46:06 +0800

    [CI/CD] local docker compose up the part of node is completed

--- node-backend/src/services/kafkaProducer.js
+++ node-backend/src/services/kafkaProducer.js
@@ -8,9 +8,6 @@ const kafka = new Kafka({
 });
 
 const producer = kafka.producer();
-const admin = kafka.admin();
-
-const requiredTopics = ["new-orders", "cancel-orders", "trade_result", "order_book_snapshot", "cancel_result"];
 
 const retryOperation = async (operation, maxRetries = 5, delay = 5000) => {
     for (let i = 0; i < maxRetries; i++) {
@@ -24,36 +21,10 @@ const retryOperation = async (operation, maxRetries = 5, delay = 5000) => {
     }
 };
 
-const createTopicsIfNotExists = async () => {
-    console.log("Attempting to create topics...");
-    await admin.connect();
-    try {
-        const existingTopics = await admin.listTopics();
-        console.log("Existing topics:", existingTopics);
-        
-        const topicsToBeCreated = requiredTopics.filter(topic => !existingTopics.includes(topic));
-        console.log("Topics to create:", topicsToBeCreated);
-        
-        if (topicsToBeCreated.length > 0) {
-            await admin.createTopics({
-                topics: topicsToBeCreated.map(topic => ({ topic, numPartitions: 1, replicationFactor: 1 }))
-            });
-            console.log(`Created topics: ${topicsToBeCreated.join(', ')}`);
-        } else {
-            console.log("All required topics already exist");
-        }
-    } finally {
-        await admin.disconnect();
-    }
-};
-
 export default {
     init: async () => {
-        await retryOperation(async () => {
-            await createTopicsIfNotExists();
-            await producer.connect();
-            console.log("Kafka producer connected and topics created");
-        });
+        await producer.connect();
+        console.log("Kafka producer connected");
     },
 
     sendMessage: async (topic, message) => {
