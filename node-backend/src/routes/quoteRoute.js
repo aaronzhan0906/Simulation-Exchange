1954135b3cf09adbe7ecdd9d27578749fd1d4070
ChengTing <aaronzhan0906@gmail.com>
2024-08-18 23:01:13 +0800

    [CHORE] change ws

--- node-backend/src/routes/quoteRoute.js
+++ node-backend/src/routes/quoteRoute.js
@@ -4,52 +4,44 @@ import WebSocket from "ws";
 
 const router = express.Router();
 
-const wsBaseUrl = process.env.WEBSOCKET_URL 
-const streamName = "btcusdt@ticker/btcusdt@depth";
+const wsBaseUrl = process.env.WEBSOCKET_URL;
+const streamName = "btcusdt@ticker";  // "/btcusdt@depth"
 const wsUrl = `${wsBaseUrl}?streams=${streamName}`;
-const btcusdtWs = new WebSocket(wsUrl)
+const btcusdtWs = new WebSocket(wsUrl);
 
 let latestTickerData = null;
-// let latestDepthData = { bids: {}, asks: {}};
-
-// store 15 seconds data
-const dataBuffer = [];
-const BUFFER_SIZE = 15; 
 
 // broadcast function
-function broadcastMessage(type ,data) {
-    wss.clients.forEach((client)=> {
-        if (client.readyState === WebSocket.OPEN){
-            client.send(JSON.stringify({type ,data}));
+function broadcastMessage(type, data) {
+    wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({ type, data }));
         }
     });
 }
 
-function processBufferData(newData) {
-    dataBuffer.push(newData);
-    if (dataBuffer.length > BUFFER_SIZE) {
-        dataBuffer.shift();
-    }
-    return dataBuffer[dataBuffer.length - 1]; // 返回最後一個元素
-}
-
-
-// get ticker and order book from binance wss
+// get ticker from binance wss
 btcusdtWs.on("message", (data) => {
     const parsedData = JSON.parse(data);
-    const { stream, data:streamData } = parsedData;
+    const { stream, data: streamData } = parsedData;
 
     // ticker
-    if ( stream === "btcusdt@ticker") {
+    if (stream === "btcusdt@ticker") {
         latestTickerData = {
-            Symbol: streamData.s,
+            symbol: streamData.s,
             price: streamData.c,
             priceChangePercent: streamData.P,
-        }
-        const processedData = processBufferData(latestTickerData);
-        broadcastMessage("ticker", processedData);
- }
-    // } else if (stream === "btcusdt@depth") {
+        };
+        broadcastMessage("ticker", latestTickerData);
+    }
+});
+
+btcusdtWs.on("error", (error) => {
+    console.error("Websocket error:", error);
+});
+
+export default router;
+// } else if (stream === "btcusdt@depth") {
     //     // renew bid
     //     streamData.b.forEach(([price, quantity]) => {
     //         if (parseFloat(quantity) === 0){
@@ -70,11 +62,7 @@ btcusdtWs.on("message", (data) => {
     //     const processedData = processBufferData(latestDepthData);
     //     broadcastMessage("orderBook", processedData)
     // }
-})
 
-btcusdtWs.on("error",(error)=>{
-    console.error("Websocket error:", error)
-})
 // function processOrderBookData(){
 //     const myExchangeData = getMyExchangeOrderBook();
     
@@ -96,4 +84,3 @@ btcusdtWs.on("error",(error)=>{
 // }
 
 // export 
-export default router;
