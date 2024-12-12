76d377be28c2c5c2cb757d76e53d6776ba04c983
ChengTing <aaronzhan0906@gmail.com>
2024-12-12 23:19:11 +0800

    [REFACTOR] Configure typescript and jest setting to migrate from JS to TS.

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,6 @@ export default {
   moduleDirectories: ["node_modules", "node-backend", "src"], // Specifies additional locations to search when resolving modules
   roots: ["<rootDir>/node-backend"], // Specifies the root directory for Jest to search for tests
   testEnvironment: "node", // The tests will run in a Node.js environment
+  testMatch: ["**/node-backend/test/**/*.test.js"], // Pattern to match test file
+  testPathIgnorePatterns: ["/dist/"] //  Exclude compiled files in dist folder
 };
